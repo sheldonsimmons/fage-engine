7c76f3c8dbfa4c3907028e70d024d589b91cbe0b
Sheldon Simmons <sheldonsimmons@Sheldons-MacBook-Pro.local>
2026-06-24 21:24:45 -0500

    Route dashboard links to operate view

--- frontend/js/onboarding.js
+++ frontend/js/onboarding.js
@@ -22,7 +22,7 @@ const TRIAL_PROXY = IS_TRIAL
   : "";
 
 function goToDashboard() {
-  window.location.href = IS_TRIAL ? "/workspace.html" : "/";
+  window.location.href = "/operate.html";
 }
 
 // Pre-fill known trial fields on page load
